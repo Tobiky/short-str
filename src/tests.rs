2258f2fda30b8e9d36f09f42bbec5bef879a78f1
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-05-19 17:31:14 +0200

    test: verify necessary assumptions for ShortStr correctness

---
+++ src/tests.rs
@@ -0,0 +1,40 @@
+use crate::ShStr;
+
+mod assumptions {
+    use crate::{CoveringInt, ShStr};
+
+    #[test]
+    /// Verify that layout is indeed ptr then len
+    fn verify_layout() {
+        let test = "test";
+        let [_, len] = unsafe { core::mem::transmute::<&str, [usize; 2]>(test) };
+        assert_eq!(len, 4);
+    }
+
+    #[test]
+    /// Verify that size is indeed 2x usize
+    fn verify_size() {
+        const STR_SIZE: usize = size_of::<&str>();
+        const DUSIZE_SIZE: usize = size_of::<[usize; 2]>();
+        const SHSTR_SIZE: usize = size_of::<ShStr>();
+        const COVER_SIZE: usize = size_of::<CoveringInt>();
+        assert_eq!(
+            STR_SIZE, DUSIZE_SIZE,
+            "expected &str to have the same size as [usize; 2] ({} vs. {})",
+            STR_SIZE, DUSIZE_SIZE
+        );
+        assert_eq!(
+            SHSTR_SIZE, DUSIZE_SIZE,
+            "expected ShortStr/ShStr to have the same size as &str and [usize; 2] ({} vs. {})",
+            SHSTR_SIZE, DUSIZE_SIZE
+        );
+        assert_eq!(
+            COVER_SIZE,
+            SHSTR_SIZE,
+            "expected CoveringInt ({}) to have the same size as ShortStr/ShStr ({} vs. {})",
+            core::any::type_name::<CoveringInt>(),
+            COVER_SIZE,
+            SHSTR_SIZE
+        )
+    }
+}
