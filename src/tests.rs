dfb443780f6c718d45c6de4db91e4dde2e897778
Tobiky <12644606+Tobiky@users.noreply.github.com>
2025-05-24 12:41:04 +0200

    fix: Remove accidental addition from cherry-pick

--- src/tests.rs
+++ src/tests.rs
@@ -93,10 +93,3 @@ fn empty_str_inline() {
         "expected converted empty &str to be equal to constant empty ShortStr"
     );
 }
-
-#[test]
-fn inline_str_upper_slice_length() {
-    let a = ShStr::from("abc");
-    let b = a[1..];
-    assert_eq!(b.len(), a[1..].len());
-}
